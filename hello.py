e8f28109bd47ae0193cb53987b4a43aba6490cad
bugakov <ebugakov@rnds.pro>
2019-05-06 10:35:51 +0300

    dev

--- hello.py
+++ hello.py
@@ -1,11 +1,15 @@
 import config
 import requests
+import logging
 from flask import Flask
 
 app = Flask(__name__)
 
-
+ 
+# add filemode="w" to overwrite
+logging.basicConfig(filename="requests.log", level=logging.INFO)
 
 @app.route('/')
 def hello_world(r):
+    logging.info(r)
     return config.CHECK_RESPONSE
